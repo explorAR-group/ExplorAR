9ce668972dd472a27c7fb76e90acf4b7a853f836
Daniel L <daniellanoff@gmail.com>
2019-05-02 12:17:40 -0400

    base viro

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -8,10 +8,6 @@ import { ViroARScene, ViroText, ViroConstants } from "react-viro";
 
 import axios from "axios";
 
-componentDidMount(){
-  
-}
-
 export default class HelloWorldSceneAR extends Component {
   constructor() {
     super();
@@ -25,14 +21,24 @@ export default class HelloWorldSceneAR extends Component {
     this._onInitialized = this._onInitialized.bind(this);
   }
 
+  async componentDidMount() {
+    let x = "5";
+
+    const { data } = await axios.get(
+      "https://api.binance.com/api/v1/depth?symbol=LTCBTC"
+    );
+
+    const newData = { ask: data.asks[0][0], bid: data.bids[0][0] };
+    let newTest = String(newData.ask);
+    console.log(newData, "!!!!!!!!!!!!!!!!");
+    this.setState({ text: newTest });
+  }
+
   render() {
-
-  
-
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized}>
         <ViroText
-          text={newTest && newTest}
+          text={this.state.text}
           scale={[0.5, 0.5, 0.5]}
           position={[0, 0, -1]}
           style={styles.helloWorldTextStyle}
@@ -43,9 +49,9 @@ export default class HelloWorldSceneAR extends Component {
 
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
-      this.setState({
-        text: "Hello World!"
-      });
+      // this.setState({
+      //   text: "Hello World!"
+      // });
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
     }
