5eed195473295c74b21a6a357541ed2c638b21a9
Peter Burger <peterburger@gmail.com>
2019-05-03 18:15:25 -0400

    feature-login-part1

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   AppRegistry,
   Text,
@@ -15,22 +15,22 @@ import {
   StyleSheet,
   PixelRatio,
   TouchableHighlight
-} from 'react-native';
-
-import { ViroARSceneNavigator } from 'react-viro';
+} from "react-native";
+import AuthForm from "./js/components/authForm";
+import { ViroARSceneNavigator } from "react-viro";
 
 /*
  TODO: Insert your API key below
  */
 var sharedProps = {
-  apiKey: 'C63BC372-68BB-4F10-B21A-7EC8E1ABFFC0'
+  apiKey: "C63BC372-68BB-4F10-B21A-7EC8E1ABFFC0"
 };
 
 // Sets the default scene you want for AR and VR
-var InitialARScene = require('./js/HelloWorldSceneAR');
+var InitialARScene = require("./js/HelloWorldSceneAR");
 
-var UNSET = 'UNSET';
-var AR_NAVIGATOR_TYPE = 'AR';
+var UNSET = "UNSET";
+var AR_NAVIGATOR_TYPE = "AR";
 
 // This determines which type of experience to launch in, or UNSET, if the user should
 // be presented with a choice of AR or VR. By default, we offer the user a choice.
@@ -67,6 +67,9 @@ export default class ViroSample extends Component {
     return (
       <View style={localStyles.outer}>
         <View style={localStyles.inner}>
+          <AuthForm />
+        </View>
+        <View style={localStyles.inner}>
           <Text style={localStyles.titleText}>
             Choose your desired experience:
           </Text>
@@ -74,7 +77,7 @@ export default class ViroSample extends Component {
           <TouchableHighlight
             style={localStyles.buttons}
             onPress={this._getExperienceButtonOnPress(AR_NAVIGATOR_TYPE)}
-            underlayColor={'#68a0ff'}
+            underlayColor={"#68a0ff"}
           >
             <Text style={localStyles.buttonText}>AR</Text>
           </TouchableHighlight>
@@ -114,30 +117,30 @@ export default class ViroSample extends Component {
 var localStyles = StyleSheet.create({
   viroContainer: {
     flex: 1,
-    backgroundColor: 'black'
+    backgroundColor: "black"
   },
   outer: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: 'black'
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "black"
   },
   inner: {
     flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    backgroundColor: 'black'
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "black"
   },
   titleText: {
     paddingTop: 30,
     paddingBottom: 20,
-    color: '#fff',
-    textAlign: 'center',
+    color: "#fff",
+    textAlign: "center",
     fontSize: 25
   },
   buttonText: {
-    color: '#fff',
-    textAlign: 'center',
+    color: "#fff",
+    textAlign: "center",
     fontSize: 20
   },
   buttons: {
@@ -147,10 +150,10 @@ var localStyles = StyleSheet.create({
     paddingBottom: 20,
     marginTop: 10,
     marginBottom: 10,
-    backgroundColor: '#68a0cf',
+    backgroundColor: "#68a0cf",
     borderRadius: 10,
     borderWidth: 1,
-    borderColor: '#fff'
+    borderColor: "#fff"
   },
   exitButton: {
     height: 50,
@@ -159,10 +162,10 @@ var localStyles = StyleSheet.create({
     paddingBottom: 10,
     marginTop: 10,
     marginBottom: 10,
-    backgroundColor: '#68a0cf',
+    backgroundColor: "#68a0cf",
     borderRadius: 10,
     borderWidth: 1,
-    borderColor: '#fff'
+    borderColor: "#fff"
   }
 });
 
